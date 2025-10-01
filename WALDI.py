46ad99bafab5015adc6bdc8ccd94e12f157ffc23
Waldirmdhn <waldirmdhn123@gmail.com>
2025-10-01 13:00:31 +0700

    Add files via upload

---
+++ WALDI.py
@@ -0,0 +1,13 @@
+nama_depan = "Waldi"
+nama_belakang = 'Ramadhan'
+nama_lengkap = nama_depan + ' ' + nama_belakang
+usia = '21'
+alamat = 'PARITTIGA'
+kata_mutiara = "Perbaiki hari ini untuk hari esok yang lebih baik"
+
+print(nama_lengkap, '(' + usia + ')', ',', 'dari', alamat, ', kata mutiara:', kata_mutiara)
+
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(usia))
+print('Tipe dari alamat:', type(alamat))
+print('Tipe dari kata_mutiara:', type(kata_mutiara))
---
+++ a = 5j waldi.py
@@ -0,0 +1,8 @@
+a = 5j
+b = 10j
+c = a + b
+
+print(a, '+', b, '=', c)
+print('Tipe dari a:', type(a))
+print('Tipe dari b:', type(b))
+print('Tipe dari c:', type(c))
